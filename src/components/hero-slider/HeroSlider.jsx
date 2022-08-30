66cb9f7e2cf40cb718095089bb441aae8862fd4e
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-31 00:07:43 +0530

    Complete header section

---
+++ src/components/hero-slider/HeroSlider.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./slider.css";
+
+import { Container } from "reactstrap";
+import Slider from "react-slick";
+import { sliderData } from "../../assets/data/slider";
+import "./slider.css";
+
+const HeroSlider = () => {
+    const settings = {
+      infinite: true,
+      speed: 800,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    };
+
+  return (
+    <section className="section">
+      <Container>
+        <Slider {...settings}>
+          {sliderData.map((item) => (
+            <div key={item.id}>
+              <div className="slider__wrapper d-flex align-items-center justify-content-between pt-5">
+                <div className="slider__content w-50 ps-2">
+                  <h2 className="mb-3 ">{item.title}</h2>
+                  <p>{item.desc}</p>
+                  <button className="btn-slider">Explore Food</button>
+                </div>
+
+                <div className="slider__img w-50">
+                  <img src={item.imgUrl} alt="" className="w-100" />
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSlider;
